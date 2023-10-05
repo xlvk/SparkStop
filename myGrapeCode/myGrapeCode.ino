5f91669840f2e04737d0302acbde6f55c6fe406c
fatabbas <fatima77614@gmail.com>
2023-10-05 15:40:00 +0300

    1

---
+++ myGrapeCode/myGrapeCode.ino
@@ -0,0 +1,128 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <DHT.h>
+
+const char *ssid = "Abbas";
+const char *password = "33447465";
+
+WebServer server(80);
+DHT dht(26, DHT11);
+const int mqPin = 34;
+
+void handleRoot() {
+  char msg[1500];
+
+  snprintf(msg, 1500,
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='4'/>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>\
+    <title>ESP32 Sensor Server</title>\
+    <style>\
+    html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}\
+    h2 { font-size: 3.0rem; }\
+    p { font-size: 3.0rem; }\
+    .units { font-size: 1.2rem; }\
+    .dht-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}\
+    </style>\
+  </head>\
+  <body>\
+      <h2>ESP32 Sensor Server!</h2>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>Temperature</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-tint' style='color:#00add6;'></i>\
+        <span class='dht-labels'>Humidity</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&percnt;</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-smog' style='color:#555555;'></i>\
+        <span class='dht-labels'>CO Concentration</span>\
+        <span>%.2f</span>\
+        <sup class='units'>ppm</sup>\
+      </p>\
+  </body>\
+</html>",
+           readDHTTemperature(), readDHTHumidity(), readCOConcentration()
+          );
+  server.send(200, "text/html", msg);
+}
+
+void setup(void) {
+
+  Serial.begin(115200);
+  dht.begin();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  delay(2); // allow the CPU to switch to other tasks
+}
+
+float readDHTTemperature() {
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.println(h);
+    return h;
+  }
+}
+
+float readCOConcentration() {
+  int sensorValue = analogRead(mqPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  Serial.print("CO Concentration: ");
+  Serial.println(voltage);
+  return voltage;
+}
+
+
---
+++ views/script.js
@@ -0,0 +1,7 @@
+//for the button.
+const searchButton = document.getElementById("searchButton");
+const searchBarContainer = document.getElementById("searchBarContainer");
+
+searchButton.addEventListener("click", function() {
+  searchBarContainer.style.display = "flex";
+});
